f4c95a42b1c55641864fdb79d77fc70439330cef
MARTY FABIEN <fabien.marty@meteo.fr>
2020-07-31 17:31:03 +0200

    build: test

--- .github/workflows/apply_common_files.sh
+++ .github/workflows/apply_common_files.sh
@@ -11,6 +11,8 @@ for REPO in $(cat /tmp/repos); do
     echo ""
     cd /tmp
     git clone "https://${USERNAME}:${PASSWORD}@github.com/metwork-framework/${REPO}.git"
+    cd "${REPO}.git"
+    git checkout -b common_files_force
     rm -Rf /tmp/common
     renvtpl "${DIR}/../../common_files" /tmp/common
     cd /tmp/common
@@ -18,8 +20,18 @@ for REPO in $(cat /tmp/repos); do
     shopt -s dotglob
     rsync -av /tmp/common/ "/tmp/${REPO}/"
     cd "/tmp/${REPO}"
-    git status
-    git diff
+    find . -type f -name "*.forcedelete" |sed 's/\.forcedelete$//g' |xargs rm -rf
+    find . -type f -name "*.forcedelete" -exec rm -f {} \;
+    git add -u
+    git add --all
+    N=$(git diff --cached |wc -l)
+    if test "${N}" -gt 0; then
+        git status
+        git diff --cached
+        git commit -m "build: sync common files from github_organization_management repository"
+        git push -u origin -f common_files_force
+        "${DIR}/../../bin/create_pr.py" --title "WIP: just a test" --body "" --base=integration metwork-framework "${REPO}" common_files_force
+    fi
     echo ""
     echo ""
 done
