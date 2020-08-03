b439f4835db9cc26087a7beb1182e2a4c04d2c69
MARTY FABIEN <fabien.marty@meteo.fr>
2020-08-03 11:54:40 +0200

    feat: add copy integration branch feature

---
+++ .github/workflows/copy_integration_master.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git config --global credential.helper cache
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TMPDIR=/tmp/copy_branch.$$
+
+function cleanup {
+    rm -Rf "${TMPDIR}"
+}
+
+set -eu
+trap cleanup EXIT
+
+mkdir -p "${TMPDIR}"
+if test "${LIMIT_TO_REPO}" != ""; then
+    echo "${LIMIT_TO_REPO}" >"${TMPDIR}/repos"
+else
+    "${DIR}/../../bin/get_repos.py" --topic=integration-level-4 metwork-framework >"${TMPDIR}/repos"
+    "${DIR}/../../bin/get_repos.py" --topic=integration-level-5 metwork-framework >>"${TMPDIR}/repos"
+fi
+for REPO in $(cat "${TMPDIR}/repos"); do
+    echo "***** REPO: ${REPO} *****"
+    echo ""
+    cd "${TMPDIR}"
+    git clone "https://${USERNAME}:${PASSWORD}@github.com/metwork-framework/${REPO}.git"
+    cd "${REPO}"
+    git config user.email "metworkbot@metwork-framework.org"
+    git config user.name "metworkbot"
+    get reset --hard origin/integration
+    git push -u origin -f master
+    echo ""
+    echo ""
+done
