917cb84ad2d6ad5e925514381f626141e58070df
Fabien MARTY <fabien.marty@gmail.com>
2020-09-16 16:20:09 +0200

    Update changelogs.sh

--- .github/workflows/changelogs.sh
+++ .github/workflows/changelogs.sh
@@ -80,7 +80,7 @@ for REPO in $(cat "${TMPDIR}/repos"); do
             BRANCH=$(echo "${T}" |awk -F ';' '{print $1;}')
             PREVIOUS=$(echo "${T}" |awk -F ';' '{print $2;}')
             TAGS=$(echo "${T}" |awk -F ';' '{print $3;}')
-            changelog "${BRANCH} CHANGELOG" "origin/${BRANCH}" "origin/${PREVIOUS}" "origin/${BRANCH}" "${TAGS}" "${BRANCH}" CHANGELOG.md "${REPO}" "${BRANCH}"
+            changelog "${BRANCH} CHANGELOG" "origin/${BRANCH}" "origin/${PREVIOUS}" "origin/${BRANCH}" "${TAGS}" "${BRANCH}" CHANGELOG.md "${REPO}" "${BRANCH}" || true
             for T2 in $("${DIR}/../../bin/active_releases.py" "${DIR}/../../releases.json"); do
                 BRANCH2=$(echo "${T2}" |awk -F ';' '{print $1;}')
                 PREVIOUS2=$(echo "${T2}" |awk -F ';' '{print $2;}')
@@ -89,7 +89,7 @@ for REPO in $(cat "${TMPDIR}/repos"); do
                 if [[ ! ${BRANCH} > ${BRANCH2} ]]; then
                     continue
                 fi
-                changelog "${BRANCH2} CHANGELOG" "origin/${BRANCH2}" "origin/${PREVIOUS2}" "origin/${BRANCH2}" "${TAGS2}" "${BRANCH}" "CHANGELOG-${TITLE2}.md" "${REPO}" "${BRANCH}"
+                changelog "${BRANCH2} CHANGELOG" "origin/${BRANCH2}" "origin/${PREVIOUS2}" "origin/${BRANCH2}" "${TAGS2}" "${BRANCH}" "CHANGELOG-${TITLE2}.md" "${REPO}" "${BRANCH}" || true
             done
         done
     fi
