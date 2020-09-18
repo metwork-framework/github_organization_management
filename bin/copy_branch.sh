b6356f1948aca09695606c368e118697f3acd057
MARTY FABIEN <fabien.marty@meteo.fr>
2020-09-18 11:31:11 +0200

    feat: add copy_branch workflow

---
+++ bin/copy_branch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+git config --global credential.helper cache
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TMPDIR=/tmp/copy_branch.$$
+
+function cleanup {
+    rm -Rf "${TMPDIR}"
+}
+
+set -eu
+trap cleanup EXIT
+
+if test "${SOURCE_BRANCH:-}" = ""; then
+    echo "SOURCE_BRANCH is mandatory"
+    exit 1
+fi
+if test "${TARGET_BRANCH:-}" = ""; then
+    echo "TARGET_BRANCH is mandatory"
+    exit 1
+fi
+
+mkdir -p "${TMPDIR}"
+if test "${LIMIT_TO_REPO:-}" != ""; then
+    echo "${LIMIT_TO_REPO}" >"${TMPDIR}/repos"
+else
+    "${DIR}/get_repos.py" --topic=integration-level-4 metwork-framework >"${TMPDIR}/repos"
+    "${DIR}/get_repos.py" --topic=integration-level-5 metwork-framework >>"${TMPDIR}/repos"
+fi
+
+for REPO in $(cat "${TMPDIR}/repos"); do
+    echo "***** REPO: ${REPO} *****"
+    echo ""
+    cd "${TMPDIR}"
+    git clone "https://${USERNAME}:${PASSWORD}@github.com/metwork-framework/${REPO}.git"
+    cd "${REPO}"
+    git config user.email "metworkbot@metwork-framework.org"
+    git config user.name "metworkbot"
+    git checkout "${SOURCE_BRANCH}"
+    if test "${REMOVE_BRANCH_PROTECTION:-}" = "1"; then
+        "${DIR}/remove_branch_protection.py" metwork-framework "${REPO}" "${TARGET_BRANCH}"
+    fi
+    git push -u origin -f "${TARGET_BRANCH}"
+    echo ""
+    echo ""
+done
---
+++ bin/remove_branch_protection.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import time
+from github import Github
+
+TOKEN = os.environ['GITHUB_TOKEN']
+
+argparser = argparse.ArgumentParser(
+    description="get integration level")
+argparser.add_argument("ORG", help="organization name")
+argparser.add_argument("REPO", help="repo name")
+argparser.add_argument("BRANCH", help="branch name")
+args = argparser.parse_args()
+
+g = Github(TOKEN)
+repo = g.get_repo("%s/%s" % (args.ORG, args.REPO))
+branch = repo.get_branch(args.BRANCH)
+branch.remove_protection()
+
+# We wait a little bit
+time.sleep(5)
