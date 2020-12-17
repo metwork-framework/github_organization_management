567448015c4c6eda043b8df04806878deb73870c
MARTY FABIEN <fabien.marty@meteo.fr>
2020-12-17 11:26:42 +0100

    build: little fix

--- bin/changelogs.sh
+++ bin/changelogs.sh
@@ -15,6 +15,7 @@ function changelog {
     # $4: FILE
     # $5: REPO
     # $6: BRANCH
+    # $7: EXTRA_OPTION
     cd "${TMPDIR}"
     if ! test -d "${5}"; then
         git clone "https://${USERNAME}:${PASSWORD}@github.com/metwork-framework/${5}.git"
@@ -24,7 +25,7 @@ function changelog {
     git config user.name "metworkbot"
     git checkout "${6}" || return 0
     set -x
-    ghtc --title="${1}" --tags-regex="${3}" --include-type=FEAT --include-type=FIX --starting-rev="${2}" . >"${4}"
+    ghtc --title="${1}" --tags-regex="${3}" --include-type=FEAT --include-type=FIX --starting-rev="${2}" "${7}" . >"${4}"
     set +x
     git add -u
     git add --all
@@ -59,17 +60,17 @@ for REPO in $(cat "${TMPDIR}/repos"); do
     echo ""
     INTEGRATION_LEVEL=$("${DIR}/get_integration_level.py" "${REPO}")
     if test "${INTEGRATION_LEVEL}" = "3"; then
-        changelog CHANGELOG "" "v*" CHANGELOG.md "${REPO}" master
+        changelog CHANGELOG "" "v*" CHANGELOG.md "${REPO}" master --unreleased
     else
         BRANCH=integration
         LATEST=$("${DIR}/latest_release.py" "${DIR}/../releases.json")
-        changelog CHANGELOG "origin/${LATEST}" "no_tag_here" CHANGELOG.md "${REPO}" integration
+        changelog CHANGELOG "origin/${LATEST}" "no_tag_here" CHANGELOG.md "${REPO}" integration --unreleased
         for T in $("${DIR}/active_releases.py" "${DIR}/../releases.json"); do
             BRANCH=$(echo "${T}" |awk -F ';' '{print $1;}')
             PREVIOUS=$(echo "${T}" |awk -F ';' '{print $2;}')
             TAGS=$(echo "${T}" |awk -F ';' '{print $3;}')
             TITLE=$(echo "${T}" |awk -F ';' '{print $4;}')
-            changelog "${BRANCH} CHANGELOG" "origin/${PREVIOUS}" "${TAGS}" CHANGELOG.md "${REPO}" "${BRANCH}" || true
+            changelog "${BRANCH} CHANGELOG" "origin/${PREVIOUS}" "${TAGS}" CHANGELOG.md "${REPO}" "${BRANCH}" --unreleased || true
             #changelog "${BRANCH} CHANGELOG" "origin/integration" "origin/${PREVIOUS}" "origin/${BRANCH}" "${TAGS}" "${BRANCH}" "CHANGELOG-${TITLE}.md" "${REPO}" integration || true
             for T2 in $("${DIR}/active_releases.py" "${DIR}/../releases.json"); do
                 BRANCH2=$(echo "${T2}" |awk -F ';' '{print $1;}')
@@ -79,7 +80,7 @@ for REPO in $(cat "${TMPDIR}/repos"); do
                 if [[ ! ${BRANCH} > ${BRANCH2} ]]; then
                     continue
                 fi
-                changelog "${BRANCH2} CHANGELOG" "origin/${PREVIOUS2}" "${TAGS2}" "CHANGELOG-${TITLE2}.md" "${REPO}" "${BRANCH}" || true
+                changelog "${BRANCH2} CHANGELOG" "origin/${PREVIOUS2}" "${TAGS2}" "CHANGELOG-${TITLE2}.md" "${REPO}" "${BRANCH}" --no-unreleased || true
             done
         done
     fi
