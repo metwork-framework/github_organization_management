3135e09ac33a1d3f8d99e67b9c5516aeaf588f06
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-01-28 19:31:32 +0100

    build: fix bootstrap

--- common_files/.github/workflows/mfxxx_bootstrap.sh
+++ common_files/.github/workflows/mfxxx_bootstrap.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
-#set -eu
-set -x
+set -eu
 
 {% if "mfext-addon" in "TOPICS"|getenv|from_json %}
     {% set MFEXT_ADDON = "1" %}
-    {% set FORCED_REPO = "mfext" %}
 {% else %}
     {% set MFEXT_ADDON = "0" %}
-    {% set FORCED_REPO = REPO %}
-{% endif %}
-{% if REPO == "mfext" %}
-    {% set DEP_MODULE = "" %}
-{% else %}
-    {% if MFEXT_ADDON == "1" %}
-        {% set DEP_MODULE = "" %}
-    {% else %}
-        {% set DEP_MODULE = "mfext" %}
-    {% endif %}
 {% endif %}
 cd /src
-ls -l /opt
-mkdir -p "/opt/metwork-${MFMODULE_LOWERCASE}-${TARGET_DIR}"
-ls -l /opt
 {% if REPO == "mfext" or MFEXT_ADDON == "1" %}
-./bootstrap.sh "/opt/metwork-${MFMODULE_LOWERCASE}-${TARGET_DIR}"
+mkdir -p /opt/metwork-mfext-${TARGET_DIR}
+./bootstrap.sh /opt/metwork-mfext-${TARGET_DIR}
 {% else %}
-./bootstrap.sh /opt/metwork-{{FORCED_REPO}}-${TARGET_DIR} /opt/metwork-{{DEP_MODULE}}-${DEP_DIR}
+mkdir -p /opt/metwork-{{REPO|lower}}-${TARGET_DIR}
+./bootstrap.sh /opt/metwork-{{REPO|lower}}-${TARGET_DIR} /opt/metwork-mfext-${DEP_DIR}
 {% endif %}
 cat adm/root.mk
 env | sort
