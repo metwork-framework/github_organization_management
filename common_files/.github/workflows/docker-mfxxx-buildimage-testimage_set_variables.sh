da2ab8cbedfbfd5e37a1fa55624c0210ba906953
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-08-30 12:16:12 +0200

    build: changes for centos8 builds

--- common_files/.github/workflows/docker-mfxxx-buildimage-testimage_set_variables.sh
+++ common_files/.github/workflows/docker-mfxxx-buildimage-testimage_set_variables.sh
@@ -14,8 +14,18 @@ fi
 
 case "${GITHUB_EVENT_NAME}" in
     repository_dispatch)
-        BRANCH=${PAYLOAD_BRANCH};;
+        BRANCH=${PAYLOAD_BRANCH}
+        if [ -f .build_os ]; then
+            OS_VERSION=`cat .build_os`
+        else
+            OS_VERSION=${PAYLOAD_OS}
+        fi;;
     push)
+        if [ -f .build_os ]; then
+            OS_VERSION=`cat .build_os`
+        else
+            OS_VERSION=centos6
+        fi
         case "${GITHUB_REF}" in
             refs/heads/*)
                 BRANCH=${GITHUB_REF#refs/heads/};;
@@ -40,5 +50,6 @@ if test "${BRANCH}" = "master"; then
 fi 
 {% endif %}
 echo "::set-output name=branch::${BRANCH}"
+echo "::set-output name=os::${OS_VERSION}"
 echo "::set-output name=tag_branch::${TAG_BRANCH}"
 echo "::set-output name=tag_latest::${TAG_LATEST}"
