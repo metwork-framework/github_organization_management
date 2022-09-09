f3c83d2a3bc77984948d2fbf2fa934a6fc20dadb
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-09-09 11:50:51 +0000

    build: fix again again

--- common_files/integration_tests/0096_system_extra_dependencies/test.sh
+++ common_files/integration_tests/0096_system_extra_dependencies/test.sh
@@ -91,7 +91,6 @@ for layer in `ls`; do
             echo
             echo "=== System extra dependencies layer ${layer} ==="
             echo
-            echo "--- external dependencies ---" ${DEPS2}
             current_layer=`cat .layerapi2_label`
             DEPS1=$(layer_wrapper --layers=${current_layer} -- external_dependencies.sh |awk -F '/' '{print $NF}' |xargs)
             # We don t consider libraries available in the layer (they should not be here, probably a LD_LIBRARY_PATH issue)
@@ -102,6 +101,7 @@ for layer in `ls`; do
                     DEPS2="${DEPS2} ${lib}"
                 fi
             done
+            echo "--- external dependencies ---" ${DEPS2}
             for DEP in ${DEPS2}; do
                 FOUND=0
                 for OK_DEP in ${OK_DEPS}; do
@@ -120,7 +120,6 @@ for layer in `ls`; do
                 echo
                 RET=1
             done
-            echo "--- dependencies not found ---" ${DEPS4}
             DEPS3=$(layer_wrapper --layers=${current_layer} -- external_dependencies_not_found.sh |xargs)
             # We don t consider libraries available in the layer (they should not be here, probably a LD_LIBRARY_PATH issue)
             DEPS4=""
@@ -130,6 +129,7 @@ for layer in `ls`; do
                     DEPS4="${DEPS4} ${lib}"
                 fi
             done
+            echo "--- dependencies not found ---" ${DEPS4}
             for DEP in ${DEPS4}; do
                 FOUND=0
                 for OK_DEP in ${OK_NOT_FOUND}; do
@@ -155,6 +155,6 @@ done
 {% endif %}
 
 if test "${RET}" = "1"; then
-    echo "extra dependencies found"
+    echo "Failure of dependencies test"
     exit 1
 fi
